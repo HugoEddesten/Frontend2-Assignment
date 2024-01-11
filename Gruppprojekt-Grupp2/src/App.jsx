c4fdbbf75a20bed17282a269ac96d154ad03dad2
HugoEddesten <hugo.eddesten@yh.nackademin.se>
2024-01-11 13:41:02 +0100

    App.jsx

---
+++ Gruppprojekt-Grupp2/src/App.jsx
@@ -0,0 +1,11 @@
+import NavigationBar from './components/NavigationBar.jsx'
+
+function App() {
+    return (
+        <div>
+            <NavigationBar />
+        </div>
+    )
+}
+
+export default App
