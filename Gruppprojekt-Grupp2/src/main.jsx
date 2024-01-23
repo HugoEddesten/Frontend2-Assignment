d5acbd8d726a70d0e2406052aa067dec22c90f68
oaxelsen <ossianas@gmail.com>
2024-01-23 11:50:00 +0100

    la till typsnitt

--- Gruppprojekt-Grupp2/src/main.jsx
+++ Gruppprojekt-Grupp2/src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import header from './header.jsx'
 import './navbar.css'
+import "typeface-sevillana"
+import "typeface-dm-serif-display"
 import { BrowserRouter } from "react-router-dom"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
