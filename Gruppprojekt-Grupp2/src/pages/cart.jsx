a810a863590aa2b9cb0ae7ba09f91c67143e446f
Miriam Sporring <mirian.sporring@gmail.com>
2024-01-29 12:58:46 +0100

    textuppdatering i cart

--- Gruppprojekt-Grupp2/src/pages/cart.jsx
+++ Gruppprojekt-Grupp2/src/pages/cart.jsx
@@ -5,7 +5,7 @@ const CartPageDiv = styled.div`
     flex-direction: column;
     justify-content: space-between;
     align-items: center;
-    width: 100%;
+    width: 50%;
     margin: 2rem 0;
 `
 
@@ -20,16 +20,16 @@ const Cart = () => {
         <div>
             <CartPageDiv>
                     <CartPageContent>
-                        <h2>Varukorg</h2>
+                        <h2>Din kundvagn</h2>
                         <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatibus ex, quibusdam repudiandae, velit impedit quam et ad quia, vel incidunt nobis sequi sunt nostrum quo hic iure ratione eaque deserunt?
                         Enim dolorem a deleniti facere, officiis ducimus, quod architecto officia veritatis odio alias libero magni non facilis minus totam explicabo? Exercitationem vitae placeat recusandae doloremque praesentium et itaque deserunt tempora?
                         Repellat vel recusandae magnam nisi beatae, nulla culpa dolorum facere obcaecati voluptatem laborum quo necessitatibus, dolor fugiat exercitationem ullam iusto cum assumenda. Sint dolorum odit impedit quod praesentium! Cupiditate, incidunt.</p>
                     </CartPageContent>
                     <CartPageContent>
-                        <h2>Ok</h2>
-                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatibus ex, quibusdam repudiandae, velit impedit quam et ad quia, vel incidunt nobis sequi sunt nostrum quo hic iure ratione eaque deserunt?
-                        Enim dolorem a deleniti facere, officiis ducimus, quod architecto officia veritatis odio alias libero magni non facilis minus totam explicabo? Exercitationem vitae placeat recusandae doloremque praesentium et itaque deserunt tempora?
-                        Repellat vel recusandae magnam nisi beatae, nulla culpa dolorum facere obcaecati voluptatem laborum quo necessitatibus, dolor fugiat exercitationem ullam iusto cum assumenda. Sint dolorum odit impedit quod praesentium! Cupiditate, incidunt.</p>
+                        
+                        <h2>Du har god smak!</h2>
+                        <p>Din beställning kommer att packas och skickas till dig snarast!<br />
+                        Tjolahopp och lite mera text</p>
                     </CartPageContent>
             </CartPageDiv>
         </div>
