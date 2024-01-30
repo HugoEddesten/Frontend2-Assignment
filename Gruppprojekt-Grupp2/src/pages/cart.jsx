c9e7a496c0688e81af8d81417e0b8d39c3937688
Miriam Sporring <mirian.sporring@gmail.com>
2024-01-30 14:21:57 +0100

    css-fil tillagd för cart

--- Gruppprojekt-Grupp2/src/pages/cart.jsx
+++ Gruppprojekt-Grupp2/src/pages/cart.jsx
@@ -1,5 +1,9 @@
 import styled from "styled-components"
 import CartList from "../components/CartList"
+import '../footer.css'
+
+
+
 
 const CartPageDiv = styled.div`
     display: flex;
@@ -20,7 +24,7 @@ const CartSum = styled.p`
     display: flex;
     flex-direction: row;
     justify-content: space-between;
-    background-color: red;
+    background-color: #b5cdd7;
     align-items: center;
     width: 100%;
     margin: 2rem 0;
@@ -46,7 +50,9 @@ const Cart = () => {
             <CartPageDiv>
                     <CartPageContent>
                         <h2>Din kundvagn</h2>
+                        <button>Fortsätt handla</button>
                         <CartSum>
+                        
                             <CartBox>
                                 Bild på produkt
                             </CartBox>
@@ -57,23 +63,18 @@ const Cart = () => {
                             <input type="number" value="0" id="input"/>
                             <button id="plus">+</button>
                             <button id="remove">Remove</button>
+                            
+                            
+                            
+                            
                         </CartSum>
+                       
+                        <button>Till kassan - länk till Delivery.jsx</button>
                     </CartPageContent>
                     <CartPageContent>
+                        Kanske ingenting här?<br />
+                        Css till knapp-partiet ovan behövs.
                         
-                        <h2>Du har bra smak! Lyckad beställning - på egen sida?</h2>
-                        <p>1000 tack för din beställning. <br />
-                            Du kommer höra från oss igen när dina varor har packats och skickats. <br />
-                            Ha det bra så länge! <br /><br />
-                            Gladeliga hälsningar från Super Yarn Company <br /><br />
-                            
-                            Roliga/trevliga bilder?<br />
-                            Nåt glatt i alla fall, kanske en selfie på oss tre, för att pigga upp <br /> <br />
-
-                             Har du frågor så tveka inte att kontakta oss. <br />
-                             Du kan nå oss på e-postadressen kundservice@superyarncompany.se eller via telefon på 08-123&nbsp;45&nbsp;67. Alla vardagar 10.00-12.00, 12.30-16.00.
-                            
-                            </p>
                     </CartPageContent>
             </CartPageDiv>
         </div>
