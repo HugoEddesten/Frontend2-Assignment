747735c7a8723f5447bfc8f847a0e711e92bc4fd
Ossian Axelsen <ossianas@gmail.com>
2024-01-30 22:02:03 +0100

    la tillbaka kundvagnen

--- Gruppprojekt-Grupp2/src/pages/cart.jsx
+++ Gruppprojekt-Grupp2/src/pages/cart.jsx
@@ -52,31 +52,32 @@ const Cart = () => {
             <CartPageDiv>
                 <CartPageContent>
                     <h2>Din kundvagn</h2>
-                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatibus ex, quibusdam repudiandae, velit impedit quam et ad quia, vel incidunt nobis sequi sunt nostrum quo hic iure ratione eaque deserunt?
-                        Enim dolorem a deleniti facere, officiis ducimus, quod architecto officia veritatis odio alias libero magni non facilis minus totam explicabo? Exercitationem vitae placeat recusandae doloremque praesentium et itaque deserunt tempora?
-                        Repellat vel recusandae magnam nisi beatae, nulla culpa dolorum facere obcaecati voluptatem laborum quo necessitatibus, dolor fugiat exercitationem ullam iusto cum assumenda. Sint dolorum odit impedit quod praesentium! Cupiditate, incidunt.</p>
+                    <button class="button"><a href="./ProductPage">Fortsätt handla</a></button>
+
+                    <CartSum>
+                        <CartBox>
+                            Bild <br />
+                        </CartBox>
+                        Namn på garn, pris,
+
+                        <button class="plus_minus_button" id="minus">−</button>
+                        <input class="value_button" type="number" value="0" id="input" />
+                        <button class="plus_minus_button" id="plus">+</button>
+                        <button id="remove">Ev remove</button>
+                        <p class="quantitys_left">Kvar i lager</p>
+
+
+
+
+                    </CartSum>
+
+                    <button> <a href="./delivery.jsx">Till kassan</a></button>
                 </CartPageContent>
+
                 <CartPageContent>
+                    Kanske ingenting här?<br />
+                    Css till knapp-partiet ovan behövs.
 
-                    <h2>Du har bra smak! Lyckad beställning - på egen sida?</h2>
-                    <p>1000 tack för din beställning. <br />
-                        Du kommer höra från oss igen när dina varor har packats och skickats. <br />
-                        Ha det bra så länge! <br /><br />
-                        Gladeliga hälsningar från Super Yarn Company <br /><br />
-
-                        Roliga/trevliga bilder?<br />
-                        Nåt glatt i alla fall, kanske en selfie på oss tre, för att pigga upp <br /> <br />
-
-                        Har du frågor så tveka inte att kontakta oss. <br />
-                        Du kan nå oss på e-postadressen kundservice@hobbii.se eller via telefon på 08-525 03888. Alla vardagar 10.00-12.00, 12.30-16.00.
-
-
-
-
-
-
-
-                    </p>
                 </CartPageContent>
             </CartPageDiv>
         </div>
