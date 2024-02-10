2be5b73a82c9d8c14758e16a7f5f112f3ada8537
Ossian Axelsen <ossianas@gmail.com>
2024-02-10 17:47:22 +0100

    .

--- Gruppprojekt-Grupp2/src/pages/cart.jsx
+++ Gruppprojekt-Grupp2/src/pages/cart.jsx
@@ -4,40 +4,29 @@ import '../cart.css'
 import CartList from "../components/CartList"
 import CartItem from "../components/CartItem"
 import axios from "axios"
-import {CartContext} from "../App"
+import { CartContext } from "../App"
 
 
 const CartItems = [];
 
 const Cart = () => {
-    
     const CartProducts = useContext(CartContext);
     console.log(CartProducts);
-
-    
-
-   
-
 
     return (
         <div className="cart-content">
 
 
             <h2>Din kundvagn</h2>
-            <button className="continue-shopping-button" onClick={event => window.location.href='/ProductPage'}>⇦ Fortsätt handla</button>
+            <button className="continue-shopping-button" onClick={event => window.location.href = '/ProductPage'}>⇦ Fortsätt handla</button>
 
-                <CartList products = {CartProducts} />
-              
-              if {
-                CartProducts == 0 {
-                    console.log ("Din kundvagn är tom");
-                }
+            <CartList products={CartProducts} />
 
-              } 
-               
+            if {
+                CartProducts == 0}
+            console.log("Din kundvagn är tom");
 
-
-            <button className="checkout-button" type="submit" onClick={event => window.location.href='/Checkout'}>Till kassan</button>
+            <button className="checkout-button" type="submit" onClick={event => window.location.href = '/Checkout'}>Till kassan</button>
 
         </div>
     )
